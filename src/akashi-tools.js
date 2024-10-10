ba6aed8bb1548d60f00cef366063c26bf06fb50b
enishi-hyodo <yuki.hyodo@enishi-techno.co.jp>
2024-10-11 00:26:25 +0900

    update: ファイル分割

--- src/akashi-tools.js
+++ src/akashi-tools.js
@@ -1,18 +1,6 @@
-// パッケージ読み込み
-const dotenv = require("dotenv");
+// require
 const { program } = require("commander");
-const axios = require("axios");
-
-// .env読み込み
-dotenv.config();
-const companyId = process.env.COMPANY_ID;
-const token = process.env.API_TOKEN;
-
-// その他定数
-const _endpoint = "https://atnd.ak4.jp/api/cooperation/";
-const _api = {
-  staffs: "staffs",
-};
+const { getStaffInfo } = require("./commands");
 
 /**
  * メイン処理
@@ -30,32 +18,6 @@ function main() {
     });
 
   program.parse(process.argv);
-}
-
-/**
- * トークンから従業員情報取得
- */
-function getStaffInfo() {
-  axios
-    .get(getApiUrl(_api.staffs), {
-      params: {
-        token: token,
-        target: token,
-      },
-    })
-    .then((response) => {
-      console.log(response.data.response);
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-}
-
-/**
- * APIのURLを取得する
- */
-function getApiUrl(api) {
-  return _endpoint + companyId + "/" + api;
 }
 
 main();
---
+++ src/commands.js
@@ -0,0 +1,44 @@
+// require
+const dotenv = require("dotenv");
+const axios = require("axios");
+
+// .env読み込み
+dotenv.config();
+const companyId = process.env.COMPANY_ID;
+const token = process.env.API_TOKEN;
+
+// その他定数
+const _endpoint = "https://atnd.ak4.jp/api/cooperation/";
+const _api = {
+  staffs: "staffs",
+};
+
+/**
+ * トークンから従業員情報取得
+ */
+function getStaffInfo() {
+  axios
+    .get(_getApiUrl(_api.staffs), {
+      params: {
+        token: token,
+        target: token,
+      },
+    })
+    .then((response) => {
+      console.log(response.data.response);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+/**
+ * APIのURLを取得する
+ */
+function _getApiUrl(api) {
+  return _endpoint + companyId + "/" + api;
+}
+
+module.exports = {
+  getStaffInfo,
+};
